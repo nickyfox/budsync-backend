11e685fee6b1879dceebab37174618d4e5a0d7b6
Nicole Fox <nfox@metrolabs.ai>
2024-10-15 17:57:18 -0300

    springboot

---
+++ src/main/java/com/example/budsyncbackend/BudsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.budsyncbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudsyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudsyncBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/budsyncbackend/BudsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budsyncbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudsyncBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
